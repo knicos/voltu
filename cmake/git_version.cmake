cc02515d4d1c942fd1d8df4bbb0f87b0a2330e8d
Nicolas Pope <nwpope@utu.fi>
2019-03-31 09:38:25 +0300

    Get version from git tags and commit

---
+++ cmake/git_version.cmake
@@ -0,0 +1,31 @@
+find_package(Git QUIET REQUIRED)
+
+if (Git_FOUND)
+	#CHECK_REQUIRED_VARIABLE(GIT_EXECUTABLE)
+
+	execute_process(COMMAND
+        "${GIT_EXECUTABLE}" describe --contains HEAD
+        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+        RESULT_VARIABLE GIT_RESULT
+        OUTPUT_VARIABLE VERSION
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+        
+	string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" ftl_VERSION_MAJOR "${VERSION}")
+	string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" ftl_VERSION_MINOR "${VERSION}")
+	string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" ftl_VERSION_PATCH "${VERSION}")
+	string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" ftl_VERSION_SHA1 "${VERSION}")
+	set(FTL_VERSION "\"${ftl_VERSION_MAJOR}.${ftl_VERSION_MINOR}.${ftl_VERSION_PATCH}\"")
+
+else()
+
+	set(ftl_VERSION_MAJOR "0")
+	set(ftl_VERSION_MINOR "0")
+	set(ftl_VERSION_PATCH "0")
+	set(FTL_VERSION "\"${ftl_VERSION_MAJOR}.${ftl_VERSION_MINOR}.${ftl_VERSION_PATCH}\"")
+	
+	message(WARNING "Version could not be obtained from git")
+
+endif()
+
+
