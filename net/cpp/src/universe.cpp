73be238f67573ba4aa124103825a6b678562e606
Nicolas Pope <nwpope@utu.fi>
2019-05-03 11:09:02 +0300

    Add another lock to universe

--- net/cpp/src/universe.cpp
+++ net/cpp/src/universe.cpp
@@ -227,6 +227,8 @@ void Universe::_run() {
 			continue;
 		}
 
+		unique_lock<mutex> lk(net_mutex_);
+
 		//If connection request is waiting
 		for (auto l : listeners_) {
 			if (l && l->isListening()) {
