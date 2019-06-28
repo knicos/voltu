ec4c406fffdaeab9a21de481bc045bcfc80147cd
Nicolas Pope <nicolas.pope@utu.fi>
2019-06-28 19:37:06 +0300

    Resolves #103 with integration refactor

--- applications/reconstruct/src/main.cpp
+++ applications/reconstruct/src/main.cpp
@@ -9,46 +9,27 @@
 #include <ftl/config.h>
 #include <ftl/configuration.hpp>
 #include <ftl/depth_camera.hpp>
-#include <ftl/scene_rep_hash_sdf.hpp>
+#include <ftl/voxel_scene.hpp>
 #include <ftl/rgbd.hpp>
 #include <ftl/virtual_source.hpp>
 #include <ftl/rgbd/streamer.hpp>
 #include <ftl/slave.hpp>
 
-// #include <zlib.h>
-// #include <lz4.h>
-
 #include <string>
 #include <vector>
 #include <thread>
 #include <chrono>
-#include <mutex>
 
 #include <opencv2/opencv.hpp>
 #include <ftl/net/universe.hpp>
-#include <ftl/rgbd_display.hpp>
-#include <nlohmann/json.hpp>
 
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/core/utility.hpp>
-
-#include <ftl/utility/opencv_to_pcl.hpp>
 #include <ftl/registration.hpp>
 
 #ifdef WIN32
 #pragma comment(lib, "Rpcrt4.lib")
 #endif
 
-#ifdef HAVE_PCL
-#include <pcl/point_cloud.h>
-#include <pcl/common/transforms.h>
-#include <pcl/filters/uniform_sampling.h>
-#endif
-
 using ftl::net::Universe;
-using ftl::rgbd::Display;
 using std::string;
 using std::vector;
 using ftl::rgbd::Source;
@@ -57,21 +38,13 @@ using ftl::config::json_t;
 using json = nlohmann::json;
 using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
-using std::mutex;
-using std::unique_lock;
+//using std::mutex;
+//using std::unique_lock;
 
-using std::vector;
-
-using cv::Mat;
+//using cv::Mat;
 
 using ftl::registration::loadTransformations;
 using ftl::registration::saveTransformations;
-
-struct Cameras {
-	Source *source;
-	DepthCameraData gpu;
-	DepthCameraParams params;
-};
 
 static void run(ftl::Configurable *root) {
 	Universe *net = ftl::create<Universe>(root, "net");
@@ -80,110 +53,59 @@ static void run(ftl::Configurable *root) {
 	net->start();
 	net->waitConnections();
 	
-	std::vector<Cameras> inputs;
-	auto sources = ftl::createArray<Source>(root, "sources", net); //root->get<vector<json_t>>("sources");
+	// Create a vector of all input RGB-Depth sources
+	auto sources = ftl::createArray<Source>(root, "sources", net);
 
 	if (sources.size() == 0) {
 		LOG(ERROR) << "No sources configured!";
 		return;
 	}
 
-	for (int i=0; i<sources.size(); i++) {
-		Source *in = sources[i];
-		auto &cam = inputs.emplace_back();
-		cam.source = in;
-		cam.params.m_imageWidth = 0;
-	}
-
-	// TODO	move this block in its own method and add automated tests
-	//		for error cases
-	
-	std::optional<json_t> merge = root->get<json_t>("merge");
-	if (!merge) {
-		LOG(WARNING) << "Input merging not configured, using only first input in configuration";
-		inputs = { inputs[0] };
-		inputs[0].source->setPose(Eigen::Matrix4d::Identity());
-	}
-
-	if (inputs.size() > 1) {
+	// Must find pose for each source...
+	if (sources.size() > 1) {
 		std::map<std::string, Eigen::Matrix4d> transformations;
 
-		/*if ((*merge)["register"]) {
-			LOG(INFO) << "Registration requested";
-
-			ftl::registration::Registration *reg = ftl::registration::ChessboardRegistration::create(*merge);
-			for (auto &input : inputs) { 
-				while(!input.source->isReady()) { std::this_thread::sleep_for(std::chrono::milliseconds(50)); }
-				reg->addSource(input.source);
-			}
-			
-			reg->run();
-			if (reg->findTransformations(transformations)) {
-				if (!saveTransformations(string(FTL_LOCAL_CONFIG_ROOT) + "/registration.json", transformations)) {
-					LOG(ERROR) << "Error saving new registration";
-				};
-			}
-			else {
-				LOG(ERROR) << "Registration failed";
-			}
-
-			free(reg);
+		if (loadTransformations(string(FTL_LOCAL_CONFIG_ROOT) + "/registration.json", transformations)) {
+			LOG(INFO) << "Loaded camera trasformations from file";
 		}
-		else {*/
-			if (loadTransformations(string(FTL_LOCAL_CONFIG_ROOT) + "/registration.json", transformations)) {
-				LOG(INFO) << "Loaded camera trasformations from file";
-			}
-			else {
-				LOG(ERROR) << "Error loading camera transformations from file";
-			}
-		//}
+		else {
+			LOG(ERROR) << "Error loading camera transformations from file";
+		}
 
-		for (auto &input : inputs) {
-			string uri = input.source->getURI();
+		for (auto &input : sources) {
+			string uri = input->getURI();
 			auto T = transformations.find(uri);
 			if (T == transformations.end()) {
 				LOG(ERROR) << "Camera pose for " + uri + " not found in transformations";
 				LOG(WARNING) << "Using only first configured source";
 				// TODO: use target source if configured and found
-				inputs = { inputs[0] };
-				inputs[0].source->setPose(Eigen::Matrix4d::Identity());
+				sources = { sources[0] };
+				sources[0]->setPose(Eigen::Matrix4d::Identity());
 				break;
 			}
-			input.source->setPose(T->second);
+			input->setPose(T->second);
 		}
 	}
 
-	// Displays and Inputs configured
-	
-	LOG(INFO) << "Using sources:";
-	for (auto &input : inputs) { LOG(INFO) << "    " + (string) input.source->getURI(); }
-	
-	//ftl::rgbd::Display *display = ftl::create<ftl::rgbd::Display>(root, "display");
+	ftl::voxhash::SceneRep *scene = ftl::create<ftl::voxhash::SceneRep>(root, "voxelhash");
+	ftl::rgbd::Streamer *stream = ftl::create<ftl::rgbd::Streamer>(root, "stream", net);
 	ftl::rgbd::Source *virt = ftl::create<ftl::rgbd::Source>(root, "virtual", net);
 
 	auto virtimpl = new ftl::rgbd::VirtualSource(virt);
 	virt->customImplementation(virtimpl);
-
-	ftl::voxhash::SceneRep *scene = ftl::create<ftl::voxhash::SceneRep>(root, "voxelhash");
 	virtimpl->setScene(scene);
-	//display->setSource(virt);
-
-	ftl::rgbd::Streamer *stream = ftl::create<ftl::rgbd::Streamer>(root, "stream", net);
 	stream->add(virt);
-	// Also proxy all inputs
-	for (auto &in : inputs) {
-		stream->add(in.source);
+
+	for (int i=0; i<sources.size(); i++) {
+		Source *in = sources[i];
+		stream->add(in);
+		scene->addSource(in);
 	}
 
 	unsigned char frameCount = 0;
 	bool paused = false;
 
-	// Keyboard camera controls
-	//display->onKey([&paused](int key) {
-	//	if (key == 32) paused = !paused;
-	//});
-
-	int active = inputs.size();
+	int active = sources.size();
 	while (ftl::running) {
 		if (active == 0) {
 			LOG(INFO) << "Waiting for sources...";
@@ -193,69 +115,27 @@ static void run(ftl::Configurable *root) {
 		active = 0;
 
 		if (!slave.isPaused()) {
-			//net.broadcast("grab");  // To sync cameras
+			// Mark voxels as cleared
 			scene->nextFrame();
 		
-			// TODO(Nick) Improve sync further...
-			for (size_t i = 0; i < inputs.size(); i++) {
-				inputs[i].source->grab();
-			}
+			// Grab, upload frames and allocate voxel blocks
+			active = scene->upload();
 
+			// Make sure previous virtual camera frame has finished rendering
 			stream->wait();
 
-			for (size_t i = 0; i < inputs.size(); i++) {
-				if (!inputs[i].source->isReady()) {
-					inputs[i].params.m_imageWidth = 0;
-					// TODO(Nick) : Free gpu allocs if was ready before
-					continue;
-				} else {
-					auto &cam = inputs[i];
-					auto in = inputs[i].source;
-					if (cam.params.m_imageWidth == 0) {
-						cam.params.fx = in->parameters().fx;
-						cam.params.fy = in->parameters().fy;
-						cam.params.mx = -in->parameters().cx;
-						cam.params.my = -in->parameters().cy;
-						cam.params.m_imageWidth = in->parameters().width;
-						cam.params.m_imageHeight = in->parameters().height;
-						cam.params.m_sensorDepthWorldMax = in->parameters().maxDepth;
-						cam.params.m_sensorDepthWorldMin = in->parameters().minDepth;
-						cam.gpu.alloc(cam.params);
-					}
-					
-					//LOG(INFO) << in->getURI() << " loaded " << cam.params.fx;
-				}
+			// Merge new frames into the voxel structure
+			scene->integrate();
 
-				// Get the RGB-Depth frame from input
-				Source *input = inputs[i].source;
-				Mat rgb, depth;
-				input->getFrames(rgb,depth);
-				
-				active += 1;
+			// Remove any redundant voxels
+			scene->garbage();
 
-				if (depth.cols == 0) continue;
-
-				// Must be in RGBA for GPU
-				Mat rgba;
-				cv::cvtColor(rgb,rgba, cv::COLOR_BGR2BGRA);
-
-				inputs[i].params.flags = frameCount;
-
-				// Send to GPU and merge view into scene
-				inputs[i].gpu.updateParams(inputs[i].params);
-				inputs[i].gpu.updateData(depth, rgba);
-				// TODO(Nick): Use double pose
-				scene->integrate(inputs[i].source->getPose().cast<float>(), inputs[i].gpu, inputs[i].params, nullptr);
-			}
 		} else {
 			active = 1;
 		}
 
-		frameCount++;
-
+		// Start virtual camera rendering and previous frame compression
 		stream->poll();
-		//display->update();
-		//sleep_for(milliseconds(10));
 	}
 }
 
---
+++ applications/reconstruct/src/voxel_scene.cpp
@@ -0,0 +1,405 @@
+#include <ftl/voxel_scene.hpp>
+#include "compactors.hpp"
+
+using namespace ftl::voxhash;
+using ftl::rgbd::Source;
+using ftl::Configurable;
+using cv::Mat;
+
+#define 	SAFE_DELETE_ARRAY(a)   { delete [] (a); (a) = NULL; }
+
+extern "C" void resetCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+extern "C" void resetHashBucketMutexCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+extern "C" void allocCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams, const DepthCameraData& depthCameraData, const DepthCameraParams& depthCameraParams, const unsigned int* d_bitMask);
+//extern "C" void fillDecisionArrayCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams, const DepthCameraData& depthCameraData);
+//extern "C" void compactifyHashCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+//extern "C" unsigned int compactifyHashAllInOneCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+extern "C" void integrateDepthMapCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams, const DepthCameraData& depthCameraData, const DepthCameraParams& depthCameraParams);
+//extern "C" void bindInputDepthColorTextures(const DepthCameraData& depthCameraData);
+
+extern "C" void starveVoxelsKernelCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+extern "C" void garbageCollectIdentifyCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+extern "C" void garbageCollectFreeCUDA(ftl::voxhash::HashData& hashData, const ftl::voxhash::HashParams& hashParams);
+
+SceneRep::SceneRep(nlohmann::json &config) : Configurable(config), do_reset_(false), m_frameCount(0) {
+	// Allocates voxel structure on GPU
+	_create(_parametersFromConfig());
+
+	on("SDFVoxelSize", [this](const ftl::config::Event &e) {
+		do_reset_ = true;
+	});
+	on("hashNumSDFBlocks", [this](const ftl::config::Event &e) {
+		do_reset_ = true;
+	});
+	on("hashNumBuckets", [this](const ftl::config::Event &e) {
+		do_reset_ = true;
+	});
+	on("hashMaxCollisionLinkedListSize", [this](const ftl::config::Event &e) {
+		do_reset_ = true;
+	});
+	on("SDFTruncation", [this](const ftl::config::Event &e) {
+		m_hashParams.m_truncation = value("SDFTruncation", 0.1f);
+	});
+	on("SDFTruncationScale", [this](const ftl::config::Event &e) {
+		m_hashParams.m_truncScale = value("SDFTruncationScale", 0.01f);
+	});
+	on("SDFMaxIntegrationDistance", [this](const ftl::config::Event &e) {
+		m_hashParams.m_maxIntegrationDistance = value("SDFMaxIntegrationDistance", 10.0f);
+	});
+}
+
+SceneRep::~SceneRep() {
+	_destroy();
+}
+
+void SceneRep::addSource(ftl::rgbd::Source *src) {
+	auto &cam = cameras_.emplace_back();
+	cam.source = src;
+	cam.params.m_imageWidth = 0;
+}
+
+int SceneRep::upload() {
+	int active = 0;
+
+	for (size_t i=0; i<cameras_.size(); ++i) {
+		cameras_[i].source->grab();
+	}
+
+	for (size_t i=0; i<cameras_.size(); ++i) {
+		auto &cam = cameras_[i];
+
+		if (!cam.source->isReady()) {
+			cam.params.m_imageWidth = 0;
+			// TODO(Nick) : Free gpu allocs if was ready before
+			continue;
+		} else {
+			auto in = cam.source;
+			// Only now do we have camera parameters for allocations...
+			if (cam.params.m_imageWidth == 0) {
+				cam.params.fx = in->parameters().fx;
+				cam.params.fy = in->parameters().fy;
+				cam.params.mx = -in->parameters().cx;
+				cam.params.my = -in->parameters().cy;
+				cam.params.m_imageWidth = in->parameters().width;
+				cam.params.m_imageHeight = in->parameters().height;
+				cam.params.m_sensorDepthWorldMax = in->parameters().maxDepth;
+				cam.params.m_sensorDepthWorldMin = in->parameters().minDepth;
+				cam.gpu.alloc(cam.params);
+			}
+		}
+
+		// Get the RGB-Depth frame from input
+		Source *input = cam.source;
+		Mat rgb, depth;
+
+		// TODO(Nick) Direct GPU upload to save copy
+		input->getFrames(rgb,depth);
+		
+		active += 1;
+
+		if (depth.cols == 0) continue;
+
+		// Must be in RGBA for GPU
+		Mat rgba;
+		cv::cvtColor(rgb,rgba, cv::COLOR_BGR2BGRA);
+
+		cam.params.flags = m_frameCount;
+
+		// Send to GPU and merge view into scene
+		cam.gpu.updateParams(cam.params);
+		cam.gpu.updateData(depth, rgba);
+
+		setLastRigidTransform(input->getPose().cast<float>());
+
+		//make the rigid transform available on the GPU
+		m_hashData.updateParams(m_hashParams);
+
+		//allocate all hash blocks which are corresponding to depth map entries
+		_alloc(cam.gpu, cam.params, nullptr);
+	}
+
+	return active;
+}
+
+void SceneRep::integrate() {
+	for (size_t i=0; i<cameras_.size(); ++i) {
+		auto &cam = cameras_[i];
+
+		setLastRigidTransform(cam.source->getPose().cast<float>());
+		m_hashData.updateParams(m_hashParams);
+
+		//generate a linear hash array with only occupied entries
+		_compactifyVisible();
+
+		//volumetrically integrate the depth data into the depth SDFBlocks
+		_integrateDepthMap(cam.gpu, cam.params);
+
+		//_garbageCollect(cam.gpu);
+
+		m_numIntegratedFrames++;
+	}
+}
+
+void SceneRep::garbage() {
+	_compactifyAllocated();
+	_garbageCollect();
+
+}
+
+/*void SceneRep::integrate(const Eigen::Matrix4f& lastRigidTransform, const DepthCameraData& depthCameraData, const DepthCameraParams& depthCameraParams, unsigned int* d_bitMask) {
+		
+	setLastRigidTransform(lastRigidTransform);
+
+	//make the rigid transform available on the GPU
+	m_hashData.updateParams(m_hashParams);
+
+	//allocate all hash blocks which are corresponding to depth map entries
+	_alloc(depthCameraData, depthCameraParams, d_bitMask);
+
+	//generate a linear hash array with only occupied entries
+	_compactifyHashEntries();
+
+	//volumetrically integrate the depth data into the depth SDFBlocks
+	_integrateDepthMap(depthCameraData, depthCameraParams);
+
+	_garbageCollect(depthCameraData);
+
+	m_numIntegratedFrames++;
+}*/
+
+void SceneRep::setLastRigidTransform(const Eigen::Matrix4f& lastRigidTransform) {
+	m_hashParams.m_rigidTransform = MatrixConversion::toCUDA(lastRigidTransform);
+	m_hashParams.m_rigidTransformInverse = m_hashParams.m_rigidTransform.getInverse();
+}
+
+/*void SceneRep::setLastRigidTransformAndCompactify(const Eigen::Matrix4f& lastRigidTransform, const DepthCameraData& depthCameraData) {
+	setLastRigidTransform(lastRigidTransform);
+	_compactifyHashEntries();
+}*/
+
+
+const Eigen::Matrix4f SceneRep::getLastRigidTransform() const {
+	return MatrixConversion::toEigen(m_hashParams.m_rigidTransform);
+}
+
+/* Nick: To reduce weights between frames */
+void SceneRep::nextFrame() {
+	if (do_reset_) {
+		do_reset_ = false;
+		_destroy();
+		_create(_parametersFromConfig());
+	} else {
+		starveVoxelsKernelCUDA(m_hashData, m_hashParams);
+		m_numIntegratedFrames = 0;
+	}
+}
+
+//! resets the hash to the initial state (i.e., clears all data)
+void SceneRep::reset() {
+	m_numIntegratedFrames = 0;
+
+	m_hashParams.m_rigidTransform.setIdentity();
+	m_hashParams.m_rigidTransformInverse.setIdentity();
+	m_hashParams.m_numOccupiedBlocks = 0;
+	m_hashData.updateParams(m_hashParams);
+	resetCUDA(m_hashData, m_hashParams);
+}
+
+//! debug only!
+unsigned int SceneRep::getHeapFreeCount() {
+	unsigned int count;
+	cudaSafeCall(cudaMemcpy(&count, m_hashData.d_heapCounter, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+	return count+1;	//there is one more free than the address suggests (0 would be also a valid address)
+}
+
+//! debug only!
+void SceneRep::debugHash() {
+	HashEntry* hashCPU = new HashEntry[m_hashParams.m_hashBucketSize*m_hashParams.m_hashNumBuckets];
+	unsigned int* heapCPU = new unsigned int[m_hashParams.m_numSDFBlocks];
+	unsigned int heapCounterCPU;
+
+	cudaSafeCall(cudaMemcpy(&heapCounterCPU, m_hashData.d_heapCounter, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+	heapCounterCPU++;	//points to the first free entry: number of blocks is one more
+
+	cudaSafeCall(cudaMemcpy(heapCPU, m_hashData.d_heap, sizeof(unsigned int)*m_hashParams.m_numSDFBlocks, cudaMemcpyDeviceToHost));
+	cudaSafeCall(cudaMemcpy(hashCPU, m_hashData.d_hash, sizeof(HashEntry)*m_hashParams.m_hashBucketSize*m_hashParams.m_hashNumBuckets, cudaMemcpyDeviceToHost));
+
+	//Check for duplicates
+	class myint3Voxel {
+	public:
+		myint3Voxel() {}
+		~myint3Voxel() {}
+		bool operator<(const myint3Voxel& other) const {
+			if (x == other.x) {
+				if (y == other.y) {
+					return z < other.z;
+				}
+				return y < other.y;
+			}
+			return x < other.x;
+		}
+
+		bool operator==(const myint3Voxel& other) const {
+			return x == other.x && y == other.y && z == other.z;
+		}
+
+		int x,y,z, i;
+		int offset;
+		int ptr;
+	}; 
+
+
+	std::unordered_set<unsigned int> pointersFreeHash;
+	std::vector<int> pointersFreeVec(m_hashParams.m_numSDFBlocks, 0);  // CHECK Nick Changed to int from unsigned in
+	for (unsigned int i = 0; i < heapCounterCPU; i++) {
+		pointersFreeHash.insert(heapCPU[i]);
+		pointersFreeVec[heapCPU[i]] = FREE_ENTRY;
+	}
+	if (pointersFreeHash.size() != heapCounterCPU) {
+		throw std::runtime_error("ERROR: duplicate free pointers in heap array");
+	}
+		
+
+	unsigned int numOccupied = 0;
+	unsigned int numMinusOne = 0;
+	//unsigned int listOverallFound = 0;
+
+	std::list<myint3Voxel> l;
+	//std::vector<myint3Voxel> v;
+	
+	for (unsigned int i = 0; i < m_hashParams.m_hashBucketSize*m_hashParams.m_hashNumBuckets; i++) {
+		if (hashCPU[i].ptr == -1) {
+			numMinusOne++;
+		}
+
+		if (hashCPU[i].ptr != -2) {
+			numOccupied++;	// != FREE_ENTRY
+			myint3Voxel a;	
+			a.x = hashCPU[i].pos.x;
+			a.y = hashCPU[i].pos.y;
+			a.z = hashCPU[i].pos.z;
+			l.push_back(a);
+			//v.push_back(a);
+
+			unsigned int linearBlockSize = m_hashParams.m_SDFBlockSize*m_hashParams.m_SDFBlockSize*m_hashParams.m_SDFBlockSize;
+			if (pointersFreeHash.find(hashCPU[i].ptr / linearBlockSize) != pointersFreeHash.end()) {
+				throw std::runtime_error("ERROR: ptr is on free heap, but also marked as an allocated entry");
+			}
+			pointersFreeVec[hashCPU[i].ptr / linearBlockSize] = LOCK_ENTRY;
+		}
+	}
+
+	unsigned int numHeapFree = 0;
+	unsigned int numHeapOccupied = 0;
+	for (unsigned int i = 0; i < m_hashParams.m_numSDFBlocks; i++) {
+		if		(pointersFreeVec[i] == FREE_ENTRY) numHeapFree++;
+		else if (pointersFreeVec[i] == LOCK_ENTRY) numHeapOccupied++;
+		else {
+			throw std::runtime_error("memory leak detected: neither free nor allocated");
+		}
+	}
+	if (numHeapFree + numHeapOccupied == m_hashParams.m_numSDFBlocks) std::cout << "HEAP OK!" << std::endl;
+	else throw std::runtime_error("HEAP CORRUPTED");
+
+	l.sort();
+	size_t sizeBefore = l.size();
+	l.unique();
+	size_t sizeAfter = l.size();
+
+
+	std::cout << "diff: " << sizeBefore - sizeAfter << std::endl;
+	std::cout << "minOne: " << numMinusOne << std::endl;
+	std::cout << "numOccupied: " << numOccupied << "\t numFree: " << getHeapFreeCount() << std::endl;
+	std::cout << "numOccupied + free: " << numOccupied + getHeapFreeCount() << std::endl;
+	std::cout << "numInFrustum: " << m_hashParams.m_numOccupiedBlocks << std::endl;
+
+	SAFE_DELETE_ARRAY(heapCPU);
+	SAFE_DELETE_ARRAY(hashCPU);
+
+	//getchar();
+}
+
+HashParams SceneRep::_parametersFromConfig() {
+	//auto &cfg = ftl::config::resolve(config);
+	HashParams params;
+	// First camera view is set to identity pose to be at the centre of
+	// the virtual coordinate space.
+	params.m_rigidTransform.setIdentity();
+	params.m_rigidTransformInverse.setIdentity();
+	params.m_hashNumBuckets = value("hashNumBuckets", 100000);
+	params.m_hashBucketSize = HASH_BUCKET_SIZE;
+	params.m_hashMaxCollisionLinkedListSize = value("hashMaxCollisionLinkedListSize", 7);
+	params.m_SDFBlockSize = SDF_BLOCK_SIZE;
+	params.m_numSDFBlocks = value("hashNumSDFBlocks",500000);
+	params.m_virtualVoxelSize = value("SDFVoxelSize", 0.006f);
+	params.m_maxIntegrationDistance = value("SDFMaxIntegrationDistance", 10.0f);
+	params.m_truncation = value("SDFTruncation", 0.1f);
+	params.m_truncScale = value("SDFTruncationScale", 0.01f);
+	params.m_integrationWeightSample = value("SDFIntegrationWeightSample", 10);
+	params.m_integrationWeightMax = value("SDFIntegrationWeightMax", 255);
+	// Note (Nick): We are not streaming voxels in/out of GPU
+	//params.m_streamingVoxelExtents = MatrixConversion::toCUDA(gas.s_streamingVoxelExtents);
+	//params.m_streamingGridDimensions = MatrixConversion::toCUDA(gas.s_streamingGridDimensions);
+	//params.m_streamingMinGridPos = MatrixConversion::toCUDA(gas.s_streamingMinGridPos);
+	//params.m_streamingInitialChunkListSize = gas.s_streamingInitialChunkListSize;
+	return params;
+}
+
+void SceneRep::_create(const HashParams& params) {
+	m_hashParams = params;
+	m_hashData.allocate(m_hashParams);
+
+	reset();
+}
+
+void SceneRep::_destroy() {
+	m_hashData.free();
+}
+
+void SceneRep::_alloc(const DepthCameraData& depthCameraData, const DepthCameraParams& depthCameraParams, const unsigned int* d_bitMask) {
+	// NOTE (nick): We might want this later...
+	if (true) {
+		//allocate until all blocks are allocated
+		unsigned int prevFree = getHeapFreeCount();
+		while (1) {
+			resetHashBucketMutexCUDA(m_hashData, m_hashParams);
+			allocCUDA(m_hashData, m_hashParams, depthCameraData, depthCameraParams, d_bitMask);
+
+			unsigned int currFree = getHeapFreeCount();
+
+			if (prevFree != currFree) {
+				prevFree = currFree;
+			}
+			else {
+				break;
+			}
+		}
+	}
+	else {
+		//this version is faster, but it doesn't guarantee that all blocks are allocated (staggers alloc to the next frame)
+		resetHashBucketMutexCUDA(m_hashData, m_hashParams);
+		allocCUDA(m_hashData, m_hashParams, depthCameraData, depthCameraParams, d_bitMask);
+	}
+}
+
+
+void SceneRep::_compactifyVisible() { //const DepthCameraData& depthCameraData) {
+	m_hashParams.m_numOccupiedBlocks = ftl::cuda::compactifyVisible(m_hashData, m_hashParams);		//this version uses atomics over prefix sums, which has a much better performance
+	m_hashData.updateParams(m_hashParams);	//make sure numOccupiedBlocks is updated on the GPU
+}
+
+void SceneRep::_compactifyAllocated() {
+	m_hashParams.m_numOccupiedBlocks = ftl::cuda::compactifyAllocated(m_hashData, m_hashParams);		//this version uses atomics over prefix sums, which has a much better performance
+	std::cout << "Occ blocks = " << m_hashParams.m_numOccupiedBlocks << std::endl;
+	m_hashData.updateParams(m_hashParams);	//make sure numOccupiedBlocks is updated on the GPU
+}
+
+void SceneRep::_integrateDepthMap(const DepthCameraData& depthCameraData, const DepthCameraParams& depthCameraParams) {
+	integrateDepthMapCUDA(m_hashData, m_hashParams, depthCameraData, depthCameraParams);
+}
+
+void SceneRep::_garbageCollect() {
+	garbageCollectIdentifyCUDA(m_hashData, m_hashParams);
+	resetHashBucketMutexCUDA(m_hashData, m_hashParams);	//needed if linked lists are enabled -> for memeory deletion
+	garbageCollectFreeCUDA(m_hashData, m_hashParams);
+}
