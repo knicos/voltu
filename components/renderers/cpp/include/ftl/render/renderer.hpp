ce61931a19503ca380fea047e242f41402fcb6c4
Nicolas Pope <nwpope@utu.fi>
2019-09-17 10:08:33 +0300

    Move the splat render

---
+++ components/renderers/cpp/include/ftl/render/renderer.hpp
@@ -0,0 +1,16 @@
+#ifndef _FTL_RENDER_RENDERER_HPP_
+#define _FTL_RENDER_RENDERER_HPP_
+
+namespace ftl {
+namespace render {
+
+class Renderer : public ftl::Configurable {
+    public:
+    Renderer();
+    virtual ~Renderer();
+};
+
+}
+}
+
+#endif  // _FTL_RENDER_RENDERER_HPP_
