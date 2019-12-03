1cfe66ffece8a18a1673b8fbac4e6b433be85e4e
Nicolas Pope <nwpope@utu.fi>
2019-12-03 09:03:39 +0200

    Use discon support for MLS

--- applications/reconstruct/src/main.cpp
+++ applications/reconstruct/src/main.cpp
@@ -334,7 +334,7 @@ static void run(ftl::Configurable *root) {
 	pipeline1->append<ftl::operators::CrossSupport>("cross2")->set("discon_support", true);
 	pipeline1->append<ftl::operators::CullDiscontinuity>("remove_discontinuity");
 	//pipeline1->append<ftl::operators::AggreMLS>("mls");  // Perform MLS (using smoothing channel)
-	pipeline1->append<ftl::operators::VisCrossSupport>("viscross"); //->set("enabled", false);
+	pipeline1->append<ftl::operators::VisCrossSupport>("viscross")->set("enabled", false);
 	pipeline1->append<ftl::operators::MultiViewMLS>("mvmls");
 	// Alignment
 
