0bd2f52fae0a48b15aec1b97245828120bc1d95b
Sami Spets <savasp@utu.fi>
2019-12-12 13:09:35 +0200

    Added keys.js template

---
+++ web-service/server/src/passport/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+    
+    //This needs to be changed
+    //Currently linked to Sami's google account
+    google: {
+        clientID: '', //Ends in apps.googleusercontent.com
+        clientSecret: ''
+    },
+
+    jwt: {
+        secret: ''
+    }
+}
