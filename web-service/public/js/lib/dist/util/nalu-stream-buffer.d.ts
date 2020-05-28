3f539ccfbb14430e9c9cded26478752ea4a5606c
Nicolas Pope <nicolas.pope@utu.fi>
2020-05-28 11:50:45 +0300

    Improve MP4 remux for browser

--- web-service/public/js/lib/dist/util/nalu-stream-buffer.d.ts
+++
@@ -1,7 +0,0 @@
-import NALU from './NALU';
-export default class VideoStreamBuffer {
-    private buffer;
-    clear(): void;
-    append(value: ArrayLike<number>): NALU[];
-    private mergeBuffer(value);
-}
