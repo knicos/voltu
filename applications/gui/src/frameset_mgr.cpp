76e46ab54c0a75fe2c7e5514ae7b60c05bb89263
Nicolas Pope <nwpope@utu.fi>
2020-07-28 11:00:53 +0300

    Remove old GUI

--- applications/gui/src/frameset_mgr.cpp
+++
@@ -1,9 +0,0 @@
-#include "frameset_mgr.hpp"
-#include <ftl/uri.hpp>
-
-static int frameset_counter = 0;
-
-int ftl::gui::mapToFrameset(const std::string &uri) {
-    //ftl::URI u(uri);
-    return frameset_counter++;
-}
