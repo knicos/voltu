5d16f00ae3ad83ca1497ebc6e41172865331221d
Nicolas Pope <nwpope@utu.fi>
2020-08-23 08:27:40 +0300

    Enable clipping in vision again

--- applications/vision/src/main.cpp
+++ applications/vision/src/main.cpp
@@ -191,7 +191,7 @@ static void run(ftl::Configurable *root) {
 	pipeline->append<ftl::operators::ArUco>("aruco")->value("enabled", false);
 	pipeline->append<ftl::operators::DetectAndTrack>("facedetection")->value("enabled", false);
 	pipeline->append<ftl::operators::DepthChannel>("depth");  // Ensure there is a depth channel
-	//pipeline->append<ftl::operators::ClipScene>("clipping")->value("enabled", false);
+	pipeline->append<ftl::operators::ClipScene>("clipping")->value("enabled", false);
 	pipeline->restore("vision_pipeline", { "clipping" });
 
 	auto h = creator->onFrameSet([sender,outstream,&stats_count,&latency,&frames,&stats_time,pipeline,&encodable,&previous_encodable](const ftl::data::FrameSetPtr &fs) {
