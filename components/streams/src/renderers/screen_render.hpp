f03ddbd00549416532aa107818f21ccb60e12cc8
Nicolas Pope <nicolas.pope@utu.fi>
2020-10-25 10:45:58 +0200

    Basic OpenGL renderer

--- components/streams/src/renderers/screen_render.hpp
+++ components/streams/src/renderers/screen_render.hpp
@@ -4,7 +4,7 @@
 #include <ftl/data/creators.hpp>
 #include <ftl/data/new_frameset.hpp>
 #include <ftl/render/renderer.hpp>
-#include <ftl/render/CUDARender.hpp>
+#include <ftl/render/GLRender.hpp>
 #include <ftl/streams/feed.hpp>
 
 #include "../baserender.hpp"
@@ -32,11 +32,12 @@ class ScreenRender : public ftl::render::BaseSourceImpl {
 	ftl::stream::Feed *feed_;
 	ftl::stream::Feed::Filter *filter_;
 	ftl::data::FrameSetPtr input_;
-	std::unique_ptr<ftl::render::CUDARender> renderer_;
+	std::unique_ptr<ftl::render::FSRenderer> renderer_;
 	ftl::Configurable *intrinsics_;
 	uint32_t my_id_;
 	ftl::operators::Graph *post_pipe_;
 	std::atomic_flag calibration_uptodate_;
+	std::string rendtype_;
 
 	/*struct AudioMixerMapping {
 		int64_t last_timestamp=0;
@@ -44,6 +45,10 @@ class ScreenRender : public ftl::render::BaseSourceImpl {
 	};
 
 	std::unordered_map<uint32_t, AudioMixerMapping> mixmap_;*/
+
+	void _createGLRender();
+	void _createCUDARender();
+	void _checkRenderer();
 };
 
 }
